6a3780b528508409e618f4be0912438cb1b3c230
khumoyun-eddie <eddie.freelance21@gmail.com>
2023-09-13 11:11:14 +0900

    week3-lab-1

--- ROS-scripts.sh
+++ ROS-scripts.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-# Run this command on every new shell you open to have access to the ROS 2 commands
+# C++ experiment code
 source /opt/ros/humble/setup.bash
+ros2 run demo_nodes_cpp talker
 
-# If you don’t want to have to source the setup file every time you open a new shell (skipping task 1), then you can add the command to your shell startup script:
-echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
-
-# Check environment variables
-printenv | grep -i ROS
+# Python experiment code
+source /opt/ros/humble/setup.bash
+ros2 run demo_nodes_py listener
