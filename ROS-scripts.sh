67f3e3a3fb0ebe10115b183bd852d3c7b47974f5
khumoyun-eddie <eddie.freelance21@gmail.com>
2023-09-13 11:24:51 +0900

    week3-lab-2

--- ROS-scripts.sh
+++ ROS-scripts.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
+# Installation
+locale  # check for UTF-8
+
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+locale  # verify settings
+
+# Enabling Ubuntu Universe
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
+# Add the ROS 2 GPG key with apt
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+# Add the repository to your sources list
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+# Updating and Upgrading apt repository caches and system.
+sudo apt update & sudo apt upgrade
+
+# Desktop Install (Recommended): ROS, RViz, demos, tutorials.
+sudo apt install ros-humble-desktop
 
 # C++ experiment code
 source /opt/ros/humble/setup.bash
